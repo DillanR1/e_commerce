d50ff5307c80664574c7bade13e875c75bce38e5
DillanR1 <dillanroby@yahoo.com>
2021-10-11 00:52:49 -0700

    UI bugs fixed. Products display to UI, material UI working

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import Grid from 'material-ui/core';
+import { Grid } from '@material-ui/core';
+import Product from './Product/Product';
 
 const products = [
-    { id: 1, name:'Shoes', description: 'Running Shoes'},
-    { id: 1, name:'Macbook', description: 'Laptop computer'},
+    { id: 1, name:'Shoes', description: 'Lifestyle Shoes', price: '$85', image:'https://images.unsplash.com/photo-1580906853149-f82f7601d205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'},
+    { id: 2, name:'Macbook', description: 'Laptop computer', price: '$1095', image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGhKO3X3BQ6cdgwi76evc9TrZvZFXAQC7pdQ&usqp=CAU' },
 ];
 
 const Products = () => {
+    return (
     <main>
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
@@ -16,6 +18,7 @@ const Products = () => {
             ))}
         </Grid>
     </main>
+    )
 }
 
 export default Products;

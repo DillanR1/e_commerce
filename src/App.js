b0a595055c4ba0f2cf30fddcea44f48024dbe824
DillanR1 <dillanroby@yahoo.com>
2021-10-11 18:15:16 -0700

    added product, fixed env key

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
+import { Products, Navbar } from "./components";
 
 // import Products from "./components/Products/Products";
 // import Navbar from "./components/Navbar/Navbar";
 
 //NOTE: Can import named components as below to make import statements neater.
 
-import { Products, Navbar } from "./components";
+const App = () => {
+  const [products, setProducts] = useState([]);
 
-function App() {
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
       <Navbar />
       <Products />
     </div>
   );
-}
+};
 
 export default App;

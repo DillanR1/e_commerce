00554d48041af3e3e0acf4ce8f18396933e0ec51
DillanR1 <dillanroby@yahoo.com>
2021-10-11 19:01:33 -0700

    Items added to UI dynamically

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,12 +3,8 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name:'Shoes', description: 'Lifestyle Shoes', price: '$85', image:'https://images.unsplash.com/photo-1580906853149-f82f7601d205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'},
-    { id: 2, name:'Macbook', description: 'Laptop computer', price: '$1095', image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGhKO3X3BQ6cdgwi76evc9TrZvZFXAQC7pdQ&usqp=CAU' },
-];
 
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
     return (
     <main className={classes.content}>
